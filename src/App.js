d1fb254fd7a6dfe32c3315bb54f0a67a33f3c2f6
Sherwin Ameri <sherwinameri@gmail.com>
2022-04-26 20:27:37 -0700

    project push

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOOOOOWWWWW!!!!
+          Learn React NOOOOOWWWWW!!!
         </a>
       </header>
     </div>
