33ebf1b40f59068cdd7b1fd33afbd931fb7c1fe8
Sherwin Ameri <sherwinameri@gmail.com>
2022-07-20 17:37:43 -0700

    new laptop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, my name is Sherwin :)
+          Hello, my name is Sherwiiiiin :)
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
